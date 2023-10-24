d65301357c98f2c5631b5bfdb6536ae12da08115
chamny20 <chamny20@naver.com>
2023-10-25 03:37:28 +0900

    design : modified ui of the kakao-button (#1)

--- src/pages/login/KakaoLogin.tsx
+++ src/pages/login/KakaoLogin.tsx
@@ -1,3 +1,6 @@
+import kakao_logo from "@/assets/kakao-talk.svg";
+import { KakaoButton } from "@/components/atoms/button/Button.styles";
+
 const KakaoLogin = () => {
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${
     import.meta.env.VITE_APP_REST_API_KEY
@@ -12,9 +15,15 @@ const KakaoLogin = () => {
   };
 
   return (
-    <>
-      <button onClick={handleKakaoLogin}>kakaologin</button>
-    </>
+    <KakaoButton onClick={handleKakaoLogin}>
+      <p>
+        <img
+          src={kakao_logo}
+          alt="Kakao"
+        />
+      </p>
+      <p>카카오로 로그인</p>
+    </KakaoButton>
   );
 };
 
