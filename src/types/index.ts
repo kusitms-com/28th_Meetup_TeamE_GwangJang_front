b0f59f1a9daf044aecbfec7ee81b73ad4dd8407d
chamny20 <chamny20@naver.com>
2023-10-31 11:40:46 +0900

    feat : add types folder

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+import { ChangeEvent } from "react";
+
+export interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  // 다른 버튼 관련 프로퍼티 추가
+}
+
+export interface InputProps {
+  value: string;
+  type: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
