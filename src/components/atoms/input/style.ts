9a0e605da8e69d3343a89258730fe58838e8ef1c
chamny20 <chamny20@naver.com>
2023-11-01 22:18:08 +0900

    chore : using defined style (#3)

--- src/components/atoms/input/style.ts
+++ src/components/atoms/input/style.ts
@@ -15,15 +15,13 @@ export const BasicInput = styled.input<InputProps>`
   //
   color: var(--Gray10_900, #212121);
   padding-left: 20px;
-  font-size: 1rem;
+  font-size: var(--text_b2);
   font-weight: 500;
 
   &::placeholder {
     color: var(--Gray7_600, #757575);
 
-    /* B2_M */
-    font-family: Pretendard;
-    font-size: 16px;
+    font-size: var(--text_b1);
     font-style: normal;
     font-weight: 500;
     line-height: 24px; /* 150% */
