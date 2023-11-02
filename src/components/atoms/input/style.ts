12c068ee6c2aa500a7891bb52c6ab73b5700dc9f
hoyyChoi <hoyeon8165@gmail.com>
2023-11-02 19:04:19 +0900

    design : border-focus

--- src/components/atoms/input/style.ts
+++ src/components/atoms/input/style.ts
@@ -4,14 +4,20 @@ import { InputProps } from ".";
 
 export const BasicInput = styled.input<InputProps>`
   box-sizing: border-box;
-  width: 370px;
+  width: 100%;
+  /* width: 370px; */
   height: 56px;
   border-radius: 5px;
   border: 1px solid var(--Gray4_300, #d9d9d9);
   outline: none;
   &:focus {
-    border: 1px solid var(--Main_Blue, #0084ff);
+    border: ${(props) =>
+      props.type === "password" && !props.errorLine
+        ? "1px solid var(--Error_50)"
+        : "1px solid var(--Main_Blue, #0084ff)"};
+    //border: 1px solid var(--Main_Blue, #0084ff);
   }
+
   //
   color: var(--Gray10_900, #212121);
   padding-left: 20px;
