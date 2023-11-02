e950f9d931e96e838abaef6fa2f5f1557c399793
chamny20 <chamny20@naver.com>
2023-11-03 01:36:28 +0900

    style : modified input-style and global.tsx file

--- src/components/atoms/input/style.ts
+++ src/components/atoms/input/style.ts
@@ -10,6 +10,7 @@ export const BasicInput = styled.input<InputProps>`
   border-radius: 5px;
   border: 1px solid var(--Gray4_300, #d9d9d9);
   outline: none;
+
   &:focus {
     border: ${(props) =>
       props.type === "password" && !props.errorLine
@@ -22,13 +23,12 @@ export const BasicInput = styled.input<InputProps>`
   color: var(--Gray10_900, #212121);
   padding-left: 20px;
   font-size: var(--text_b2);
-  padding-right: 80px;
+  /* padding-right: 80px; */
   font-weight: 500;
 
   &::placeholder {
     color: var(--Gray7_600, #757575);
-
-    font-size: var(--text_b1);
+    font-size: 1rem;
     font-style: normal;
     font-weight: 500;
     line-height: 24px; /* 150% */
