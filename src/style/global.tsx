7c12b6c2b262172e41811f5401509349566c4b7c
chamny20 <chamny20@naver.com>
2023-11-06 01:33:55 +0900

    design : modified Inner (#13)

--- src/style/global.tsx
+++ src/style/global.tsx
@@ -74,9 +74,11 @@ a{
 
 export const Inner = styled.div`
   min-width: 1000px;
-  width: 1080px;
+  width: 1200px;
   margin: 0 auto;
   background-color: white;
   justify-content: space-between;
   display: flex;
+  //
+  flex-direction: column;
 `;
