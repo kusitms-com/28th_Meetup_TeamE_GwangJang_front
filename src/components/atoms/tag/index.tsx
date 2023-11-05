038ff652ef6d83b4b0234229c71f9aa9b7a63fe9
chamny20 <chamny20@naver.com>
2023-11-06 01:35:39 +0900

    feat : add tag atom (#13)

---
+++ src/components/atoms/tag/index.tsx
@@ -0,0 +1,5 @@
+import { BlueTag } from "./style";
+
+export const TopicTag = () => {
+  return <BlueTag>일자리 · 노동</BlueTag>;
+};
---
+++ src/components/atoms/tag/style.ts
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const BlueTag = styled.div`
+  display: inline-flex;
+  padding: 3px 10px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 55px;
+  background: var(--Main_Blue, #0084ff);
+
+  //text
+  color: var(--Gray1_50, #fafafa);
+  text-align: center;
+  font-size: var(--text_cap1);
+  font-weight: 600;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.18px;
+`;
+
+export const GreenTag = styled.div`
+  display: inline-flex;
+
+  padding: 3px 10px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 55px;
+  background: var(--Main_NeonGreen);
+
+  //text
+  color: var(--Gray1_50, #fafafa);
+  text-align: center;
+  font-size: var(--text_cap1);
+  font-weight: 600;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.18px;
+`;
+
+export const OrangeTag = styled.div`
+  display: inline-flex;
+
+  padding: 3px 10px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 55px;
+  background: var(--Sub_Orange, #f90);
+  //text
+  color: var(--Gray1_50, #fafafa);
+  text-align: center;
+  font-size: var(--text_cap1);
+  font-weight: 600;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.18px;
+`;
+
+export const PurpleTag = styled.div`
+  display: inline-flex;
+
+  padding: 3px 10px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 55px;
+  background: var(--Sub_Purple, #75f);
+  //text
+  color: var(--Gray1_50, #fafafa);
+  text-align: center;
+  font-size: var(--text_cap1);
+  font-weight: 600;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.18px;
+`;
