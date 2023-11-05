967f4de7246d1594ca2073ef6f743a39951a28cb
chamny20 <chamny20@naver.com>
2023-11-06 04:00:46 +0900

    feat : add main-content molecule (#15)

---
+++ src/components/molecules/main/contentbox/index.tsx
@@ -0,0 +1,32 @@
+import { KeywordTag, TopicTag } from "@/components/atoms/tag";
+
+import { ContentContainer } from "./style";
+
+export const ContentBox = () => {
+  const imageUrl = "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg";
+
+  const containerStyle = {
+    backgroundImage: `linear-gradient(180deg, rgba(34, 34, 34, 0.2) 0%, #222 79.69%),url(${imageUrl})`,
+    backgroundSize: "cover", // 이미지를 컨테이너에 맞게 조절합니다.
+    boxShadow: "0px 15px 34px 0px rgba(207, 207, 207, 0.1)",
+  };
+
+  return (
+    <ContentContainer style={containerStyle}>
+      <div className="tag-box">
+        <div>
+          <TopicTag />
+        </div>
+        <div>
+          <KeywordTag />
+        </div>
+      </div>
+      <div className="text-box">
+        <p>기사</p>
+        <div className="content-title">
+          직접고용 위해 설립됐는데 노조탈퇴 강요…檢 SPC계열사 압수수색
+        </div>
+      </div>
+    </ContentContainer>
+  );
+};
---
+++ src/components/molecules/main/contentbox/style.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const ContentContainer = styled.div`
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 20px;
+  /* width: 384px; */
+  height: 240px;
+  border-radius: 5px;
+
+  .tag-box {
+    display: flex;
+    gap: 6px;
+  }
+
+  .text-box {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    p {
+      color: var(--Gray4_300, #d9d9d9);
+      text-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
+      font-size: 14px;
+      font-weight: 700;
+      line-height: 22px; /* 157.143% */
+      letter-spacing: -0.21px;
+    }
+
+    .content-title {
+      color: #fff;
+      text-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
+      font-size: 18px;
+      font-weight: 600;
+      line-height: 28px; /* 155.556% */
+      letter-spacing: -0.27px;
+    }
+  }
+`;
