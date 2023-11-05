71324a1955fb8b7c5e99e81a7cfa08bed133cf10
chamny20 <chamny20@naver.com>
2023-11-06 01:35:24 +0900

    feat : add see-more atom (#13)

---
+++ src/components/atoms/more/style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const MoreBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: var(--Gray7_600, #757575);
+  align-items: center;
+  font-size: 16px;
+  font-weight: 500;
+  justify-content: right;
+
+  .next-icon {
+    color: inherit;
+    font-size: 20px;
+  }
+`;
