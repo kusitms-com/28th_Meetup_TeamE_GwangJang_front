514809d211dddb83ed2219b72b7fb2c18614c10a
hoyyChoi <hoyeon8165@gmail.com>
2023-11-07 18:32:51 +0900

    feat : temporary tittle & addingTopic component

--- src/components/organisms/Home/DiscussedTopics.tsx
+++ src/components/organisms/Home/DiscussedTopics.tsx
@@ -6,26 +6,61 @@ import { topicData } from "@/dummy/topicData";
 
 const DiscussedTopics = () => {
   return (
-    <DiscussedTopicContainer>
-      {topicData.map((data, idx) => (
-        <Topic
-          title={data.title}
-          subTitles={data.subTitles}
-          idx={idx}
-          key={idx}
-        />
-      ))}
-    </DiscussedTopicContainer>
+    <Container>
+      <div className="inner title">주제</div>
+      <DiscussedTopicContainer>
+        {topicData.map((data, idx) => (
+          <Topic
+            title={data.title}
+            subTitles={data.subTitles}
+            idx={idx}
+            key={idx}
+          />
+        ))}
+      </DiscussedTopicContainer>
+      <div className="inner">
+        <div className="addingTopic">주제를 추가하고 싶어요.</div>
+      </div>
+    </Container>
   );
 };
 
 export default DiscussedTopics;
 
+const Container = styled.div`
+  background-color: var(--Gray3_200);
+  width: 100%;
+  padding-top: 50px;
+  padding-bottom: 105.5px;
+  box-sizing: border-box;
+  @media (max-width: 1200px) {
+    padding-top: 90px;
+    padding-bottom: 90px;
+  }
+  .inner {
+    width: 1200px;
+    margin: 0 auto 20px;
+  }
+  .addingTopic {
+    margin-top: 20px;
+    float: right;
+    display: inline-flex;
+    padding: 8px 14px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 55px;
+    background: var(--Gray4_300, #d9d9d9);
+    color: var(--Gray8_700, #616161);
+    text-align: center;
+    cursor: pointer;
+  }
+`;
 const DiscussedTopicContainer = styled.div`
   display: flex;
   justify-content: space-between;
   width: 1200px;
   margin: 0 auto;
+  background-color: var(--Gray3_200);
   @media (max-width: 1200px) {
     flex-flow: wrap;
     display: flex;
