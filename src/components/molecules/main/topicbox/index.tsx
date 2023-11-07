4f0474071270975db8ad81e025cc129ae321ce55
chamny20 <chamny20@naver.com>
2023-11-07 23:17:39 +0900

    feat : apply hover event (#13)

--- src/components/molecules/main/topicbox/index.tsx
+++ src/components/molecules/main/topicbox/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { TopicTag } from "@/components/atoms/tag";
 
 import { TopicContainer } from "./style";
@@ -10,15 +12,40 @@ export const TopicBox = () => {
     backgroundSize: "cover",
   };
 
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHover(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHover(false);
+  };
+
   return (
-    <TopicContainer style={containerStyle}>
-      <div>
-        <TopicTag />
-        <div className="topic-title">주 69시간 근로시간 제도 개편</div>
-      </div>
-      <div className="subscribe">
-        <p>80명</p>이 구독하고 있어요
-      </div>
+    <TopicContainer
+      style={containerStyle}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      {isHover ? (
+        <img
+          className="img-hover"
+          src={imageUrl}
+          alt=""
+        />
+      ) : (
+        <div className="topic-wrapper">
+          {" "}
+          <div>
+            <TopicTag />
+            <div className="topic-title">주 69시간 근로시간 제도 개편</div>
+          </div>
+          <div className="subscribe">
+            <p>80명</p>이 구독하고 있어요
+          </div>
+        </div>
+      )}
     </TopicContainer>
   );
 };
