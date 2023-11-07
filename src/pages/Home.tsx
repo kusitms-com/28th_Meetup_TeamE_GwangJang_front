18134c5129d1fd9c00738ebd4d2ee8cb8f20524d
hoyyChoi <hoyeon8165@gmail.com>
2023-11-07 18:53:49 +0900

    feat : add DiscussedTopics

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import BubbleChart from "@/components/organisms/Home/BubbleChart";
+import DiscussedTopics from "@/components/organisms/Home/DiscussedTopics";
 
 const Home = () => {
   return (
     <>
       <BubbleChart />
       {/* 여러가지 메인에 들어갈 organism들 */}
+      <DiscussedTopics />
     </>
   );
 };
