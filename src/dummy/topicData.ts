535f241f76f64e6c09d4c9d4789695f243f37be7
hoyyChoi <hoyeon8165@gmail.com>
2023-11-07 15:56:49 +0900

    chore : topic dummy data key

--- src/dummy/topicData.ts
+++ src/dummy/topicData.ts
@@ -1,18 +1,20 @@
+// import { discussedTopicProps } from "@/types";
+
 export const topicData = [
   {
     title: "일자리 노동1",
-    subtitle: ["SPC", "쿠팡 노동자 사망", "주 69시간 근로시간 제도 개편"],
+    subTitles: ["SPC", "쿠팡 노동자 사망", "주 69시간 근로시간 제도 개편"],
   },
   {
     title: "일자리 노동2",
-    subtitle: ["SPC", "쿠팡 노동자 사망", "주 69시간 근로시간 제도 개편"],
+    subTitles: ["SPC", "쿠팡 노동자 사망", "주 69시간 근로시간 제도 개편"],
   },
   {
     title: "일자리 노동3",
-    subtitle: ["SPC", "쿠팡 노동자 사망", "주 69시간 근로시간 제도 개편"],
+    subTitles: ["SPC", "쿠팡 노동자 사망", "주 69시간 근로시간 제도 개편"],
   },
   {
     title: "일자리 노동4",
-    subtitle: ["SPC", "쿠팡 노동자 사망", "주 69시간 근로시간 제도 개편"],
+    subTitles: ["SPC", "쿠팡 노동자 사망", "주 69시간 근로시간 제도 개편"],
   },
 ];
