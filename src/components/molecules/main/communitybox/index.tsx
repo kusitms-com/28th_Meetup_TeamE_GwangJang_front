226841f11b5ff696bd4eb68bbb62feb6ef282a69
chamny20 <chamny20@naver.com>
2023-11-08 02:57:02 +0900

    design : add ui about text and quotation (#16)

--- src/components/molecules/main/communitybox/index.tsx
+++ src/components/molecules/main/communitybox/index.tsx
@@ -1,10 +1,45 @@
+import temp from "@/assets/react.svg";
+import { KeywordTag, TopicTag } from "@/components/atoms/tag";
+
 import { CommunityContainer } from "./style";
 
 export const CommunityBox = () => {
   return (
     <CommunityContainer>
-      <div className="community-content">하얀색박스</div>
-      <div className="quotation">인용</div>
+      <div className="community-content">
+        <div className="community-tag-wrapper">
+          <TopicTag color="red" />
+          <KeywordTag color="red" />
+        </div>
+        <div className="content-wrapper">
+          <div className="content-text">
+            일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는
+            비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며,
+            강제성이 있는이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는
+            비공식적, 사적
+          </div>
+          <div className="status-info">
+            <div className="user-info">
+              <img
+                src={temp}
+                alt=""
+              />
+              <div>
+                <p className="user-nickname">광장찌개닉네임</p>
+                <p className="content-date">2023.10.24</p>
+              </div>{" "}
+            </div>
+            <div className="content-review">
+              <p>좋아요</p>
+              <p>댓글</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="quotation">
+        <div className="quot-title">인용한 콘텐츠</div>
+        <div className="quot-text">인용 텍스트어어어어어어</div>
+      </div>
     </CommunityContainer>
   );
 };
--- src/components/molecules/main/communitybox/style.ts
+++ src/components/molecules/main/communitybox/style.ts
@@ -12,14 +12,95 @@ export const CommunityContainer = styled.div`
   border-radius: 5px;
 
   .community-content {
+    padding: 20px;
     border-radius: 5px 5px 0px 0px;
     height: 70%;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+
+    .community-tag-wrapper {
+      display: flex;
+      gap: 6px;
+    }
+
+    .content-wrapper {
+      display: flex;
+      flex-direction: column;
+      gap: 18px;
+
+      .content-text {
+        height: 90px;
+        overflow: hidden;
+        color: var(--Gray9_800, #424242);
+        font-size: var(--text_b3);
+        font-weight: 400;
+        line-height: 22px; /* 157.143% */
+        letter-spacing: -0.21px;
+      }
+
+      .status-info {
+        display: flex;
+        justify-content: space-between;
+        flex-direction: row;
+        border: 1px solid blue;
+
+        .user-info {
+          display: flex;
+          gap: 10px;
+          img {
+            border-radius: 100%;
+            object-fit: cover;
+          }
+
+          .user-nickname {
+            color: var(--Gray7_600, #757575);
+            font-size: 12px;
+            line-height: 18px; /* 150% */
+            letter-spacing: -0.18px;
+          }
+
+          .content-date {
+            color: var(--Gray5_400, #bdbdbd);
+            font-size: 10px;
+            font-weight: 400;
+            line-height: 10px; /* 100% */
+            letter-spacing: -0.15px;
+          }
+        }
+
+        .content-review {
+          border: 1px solid red;
+          display: flex;
+          gap: 10px;
+        }
+      }
+    }
   }
 
   .quotation {
+    padding: 20px;
     display: flex;
+    gap: 4px;
+    flex-direction: column;
     height: 30%;
     border-radius: 0px 0px 5px 5px;
     background: var(--Gray2_100, #f5f5f5);
+
+    .quot-title {
+      color: var(--Gray6_500, #959595);
+      font-size: 10px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 14px; /* 140% */
+      letter-spacing: -0.15px;
+    }
+
+    .quot-text {
+      color: var(--Gray8_700, #616161);
+      font-size: 14px;
+      line-height: 22px; /* 157.143% */
+      letter-spacing: -0.21px;
+    }
   }
 `;
