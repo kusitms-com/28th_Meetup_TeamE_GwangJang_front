157263029cfd90ca9789c6f3bfbf5bbc8e7af76a
chamny20 <chamny20@naver.com>
2023-11-09 05:08:19 +0900

    design : modified css (#16)

--- src/components/molecules/main/communitybox/index.tsx
+++ src/components/molecules/main/communitybox/index.tsx
@@ -1,5 +1,8 @@
+import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 
+import commentImg from "@/assets/comment.svg";
+import likeImg from "@/assets/like.svg";
 import temp from "@/assets/react.svg";
 import { KeywordTag, TopicTag } from "@/components/atoms/tag";
 import { mainCommunityData } from "@/dummy/mainCommunityData";
@@ -10,86 +13,70 @@ import "swiper/css";
 
 export const CommunityBox = () => {
   return (
-    <Swiper
-      spaceBetween={24}
-      slidesPerView={3.5}
-      slidesPerGroup={3}
-      loop={true}
-      // onSlideChange={() => console.log("slide change")}
-      // onSwiper={(swiper) => console.log(swiper)}
-    >
-      {mainCommunityData.map((item, idx) => (
-        <SwiperSlide key={idx}>
-          <CommunityContainer>
-            <div className="community-content">
-              <div className="community-tag-wrapper">
-                <TopicTag color="red" />
-                <KeywordTag color="red" />
-              </div>
-              <div className="content-wrapper">
-                <div className="content-text">{item.communityText}</div>
-                <div className="status-info">
-                  <div className="user-info">
-                    <img
-                      src={temp}
-                      alt=""
-                    />
-                    <div>
-                      <p className="user-nickname">{item.nickname}</p>
-                      <p className="content-date">{item.date}</p>
-                    </div>{" "}
-                  </div>
-                  <div className="content-review">
-                    <p>좋아요{item.likeCount}</p>
-                    <p>댓글{item.commentCount}</p>
+    <TempContainer>
+      <Swiper
+        spaceBetween={24}
+        slidesPerView={3.5}
+        slidesPerGroup={3}
+        loop={true}
+        // onSlideChange={() => console.log("slide change")}
+        // onSwiper={(swiper) => console.log(swiper)}
+      >
+        {mainCommunityData.map((item, idx) => (
+          <SwiperSlide key={idx}>
+            <CommunityContainer>
+              <div className="community-content">
+                <div className="community-tag-wrapper">
+                  <TopicTag color="red" />
+                  <KeywordTag color="red" />
+                </div>
+                <div className="content-wrapper">
+                  <div className="content-text">{item.communityText}</div>
+                  <div className="status-info">
+                    <div className="user-info">
+                      <img
+                        src={temp}
+                        alt=""
+                      />
+                      <div>
+                        <p className="user-nickname">{item.nickname}</p>
+                        <p className="content-date">{item.date}</p>
+                      </div>{" "}
+                    </div>
+                    <div className="content-review">
+                      <div>
+                        <img
+                          src={likeImg}
+                          alt="좋아요"
+                        />
+                        <p>{item.likeCount}</p>
+                      </div>
+                      <div>
+                        <img
+                          src={commentImg}
+                          alt="좋아요"
+                        />
+                        <p>{item.commentCount}</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="quotation">
-              <div className="quot-title">인용한 콘텐츠</div>
-              <div className="quot-text">{item.quotText}</div>
-            </div>
-          </CommunityContainer>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-
-    // <CommunityContainer>
-    //   <div className="community-content">
-    //     <div className="community-tag-wrapper">
-    //       <TopicTag color="red" />
-    //       <KeywordTag color="red" />
-    //     </div>
-    //     <div className="content-wrapper">
-    //       <div className="content-text">
-    //         일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는
-    //         비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며,
-    //         강제성이 있는이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는
-    //         비공식적, 사적
-    //       </div>
-    //       <div className="status-info">
-    //         <div className="user-info">
-    //           <img
-    //             src={temp}
-    //             alt=""
-    //           />
-    //           <div>
-    //             <p className="user-nickname">광장찌개닉네임</p>
-    //             <p className="content-date">2023.10.24</p>
-    //           </div>{" "}
-    //         </div>
-    //         <div className="content-review">
-    //           <p>좋아요</p>
-    //           <p>댓글</p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="quotation">
-    //     <div className="quot-title">인용한 콘텐츠</div>
-    //     <div className="quot-text">인용 텍스트어어어어어어</div>
-    //   </div>
-    // </CommunityContainer>
+              <div className="quotation">
+                <div className="quot-title">인용한 콘텐츠</div>
+                <div className="quot-text">{item.quotText}</div>
+              </div>
+            </CommunityContainer>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </TempContainer>
   );
 };
+
+const TempContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  margin-left: 300px;
+  overflow: hidden;
+`;
--- src/components/organisms/Home/MainCommunity.tsx
+++ src/components/organisms/Home/MainCommunity.tsx
@@ -3,25 +3,39 @@ import styled from "styled-components";
 import { SeeMore } from "@/components/atoms/more";
 import { Title } from "@/components/atoms/title";
 import { CommunityBox } from "@/components/molecules/main/communitybox";
+import { Inner } from "@/style/global";
 
 export const MainCommunity = () => {
   return (
     <TempInner>
       <MainCommunityBack>
-        {" "}
-        <Title title="지금 가장 인기있는 커뮤니티 글" />
+        <Inner>
+          <Title title="지금 가장 인기있는 커뮤니티 글" />
+        </Inner>
+
         <CommunityBox />
-        <SeeMore text="더보기" />
+        <Inner>
+          <SeeMore text="더보기" />
+        </Inner>
       </MainCommunityBack>
     </TempInner>
   );
 };
 
-const MainCommunityBack = styled.div``;
+const MainCommunityBack = styled.div`
+  /* width: 1440px; */
+  width: 100%;
+  /* margin-left: 180px; */
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin: 0 auto;
+`;
 
 const TempInner = styled.div`
-  width: 1440px;
-  height: 560px;
   background: var(--Gray4_300, #d9d9d9);
-  margin: 0 auto;
+  height: 525px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
 `;
