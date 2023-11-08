c8a2250078d7ad082247520646c250811434d076
hoyyChoi <hoyeon8165@gmail.com>
2023-11-09 06:40:30 +0900

    fix : global inner

--- src/style/global.tsx
+++ src/style/global.tsx
@@ -74,11 +74,8 @@ a{
 
 export const Inner = styled.div`
   min-width: 1000px;
-  width: 1200px;
+  width: 1080px;
   margin: 0 auto;
-  background-color: white;
   justify-content: space-between;
   display: flex;
-  //
-  flex-direction: column;
 `;
