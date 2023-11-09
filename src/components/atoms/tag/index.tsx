e8cd0d0e9d909d2108b9ad15a14cee9509cae9aa
chamny20 <chamny20@naver.com>
2023-11-10 06:41:24 +0900

    feat : apply color in tag (#15)

--- src/components/atoms/tag/index.tsx
+++ src/components/atoms/tag/index.tsx
@@ -1,9 +1,28 @@
 import { Keyword, Tag } from "./style";
 
-export const TopicTag = ({ color }: { color: string }) => {
-  return <Tag background={color}>일자리 · 노동</Tag>;
+export const TopicTag = ({ color, category }: { color: string; category: string }) => {
+  if (category == "일자리·노동") {
+    color = "#0084FF";
+  } else if (category == "환경") {
+    color = "#1AE276";
+  } else if (category == "교육") {
+    color = "#F90";
+  } else {
+    color = "#75F";
+  }
+
+  return <Tag background={color}>{category}</Tag>;
 };
 
-export const KeywordTag = ({ color }: { color: string }) => {
-  return <Keyword color={color}>일자리 · 노동</Keyword>;
+export const KeywordTag = ({ color, category }: { color: string; category: string }) => {
+  if (category == "일자리·노동") {
+    color = "#0084FF";
+  } else if (category == "환경") {
+    color = "#1AE276";
+  } else if (category == "교육") {
+    color = "#F90";
+  } else if (category == "주거") {
+    color = "#75F";
+  }
+  return <Keyword color={color}>{category}</Keyword>;
 };
