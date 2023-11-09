9117267e4f2a98d676184c6dbb10332f8c33b7f4
chamny20 <chamny20@naver.com>
2023-11-10 04:29:24 +0900

    chore : add dummy-data (#15)

--- src/dummy/ContentData.ts
+++ src/dummy/ContentData.ts
@@ -4,29 +4,45 @@ export const contentData = [
     keyword: ["SPC", "사망", "이채민이챔"],
     type: "기사",
     title: "SPC 빵 만들다 숨진 23살…“제 딸 박선빈, 기억해주세요”",
+    imgUrl:
+      "https://images.chosun.com/resizer/XKL6ePOdAuAn81yF-ZBOY8VyQWs=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/JMI3RCEB2Y7QSIUWJKT2MT7CC4.jpg",
+    content:
+      "10월 7일 토요일 팔레스타인 무장 정파 하마스의 이스라엘 기습 공격은 전 세계를 충격에 빠뜨렸습니다. 하마스 무장 괴한들은 이스라엘 마을로 건너...",
   },
   {
     topic: "일자리 · 노동",
     keyword: ["SPC", "사망", "이채민이챔"],
     type: "기사",
     title: "직접고용 위해 설립됐는데 노조탈퇴 강요…檢 SPC계열사 압수수색",
+    imgUrl: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+    content:
+      "10월 7일 토요일 팔레스타인 무장 정파 하마스의 이스라엘 기습 공격은 전 세계를 충격에 빠뜨렸습니다. 하마스 무장 괴한들은 이스라엘 마을로 건너...",
   },
   {
     topic: "일자리 · 노동",
     keyword: ["SPC", "사망", "이채민이챔"],
     type: "기사",
     title: "SPC 빵 만들다 숨진 23살…“제 딸 박선빈, 기억해주세요”",
+    imgUrl: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+    content:
+      "10월 7일 토요일 팔레스타인 무장 정파 하마스의 이스라엘 기습 공격은 전 세계를 충격에 빠뜨렸습니다. 하마스 무장 괴한들은 이스라엘 마을로 건너...",
   },
   {
     topic: "일자리 · 노동",
     keyword: ["SPC", "사망", "이채민이챔"],
     type: "기사",
     title: "SPC 빵 만들다 숨진 23살…“제 딸 박선빈, 기억해주세요”",
+    imgUrl: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+    content:
+      "10월 7일 토요일 팔레스타인 무장 정파 하마스의 이스라엘 기습 공격은 전 세계를 충격에 빠뜨렸습니다. 하마스 무장 괴한들은 이스라엘 마을로 건너...",
   },
   {
     topic: "일자리 · 노동",
     keyword: ["SPC", "사망", "이채민이챔"],
     type: "기사",
     title: "SPC 빵 만들다 숨진 23살…“제 딸 박선빈, 기억해주세요”",
+    imgUrl: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+    content:
+      "10월 7일 토요일 팔레스타인 무장 정파 하마스의 이스라엘 기습 공격은 전 세계를 충격에 빠뜨렸습니다. 하마스 무장 괴한들은 이스라엘 마을로 건너...",
   },
 ];
