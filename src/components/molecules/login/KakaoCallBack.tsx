41561af51f11792abf59a539c622929bb85f5e96
hoyyChoi <hoyeon8165@gmail.com>
2023-11-12 04:52:57 +0900

    fix : kakao API

--- src/components/molecules/login/KakaoCallBack.tsx
+++ src/components/molecules/login/KakaoCallBack.tsx
@@ -6,38 +6,30 @@ import { SyncLoader } from "react-spinners";
 import styled from "styled-components";
 
 export const KakaoCallBack = () => {
-  const navigate = useNavigate();
-
   //인가코드
-  const CODE = new URL(window.location.href).searchParams.get("code");
-  console.log("인가코드:", CODE);
-
+  const navigate = useNavigate();
   useEffect(() => {
-    const getToken = async () => {
-      console.log("getToken 호출");
-
-      axios
-        .post(
-          "https://api.gwang-jang.co.kr/member/auth/signIn/kakao",
-          { token: CODE },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              // "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-            },
-          }
-        )
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => {
-          console.error("error:", err);
-        });
-
-      navigate("/");
-    };
-    getToken();
-  }, [CODE]);
+    const Code = new URL(window.location.href).searchParams.get("code");
+    console.log("인가코드:", Code);
+    axios
+      .post(
+        "https://api.gwang-jang.co.kr/member/auth/signIn/kakao",
+        { token: Code },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            // "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.error("error:", err);
+      });
+  }, [navigate]);
 
   return (
     <LoadingBox>
