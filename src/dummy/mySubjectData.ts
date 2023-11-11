523f089ee23abb3da4ead21ccb576ae774c14ec8
chamny20 <chamny20@naver.com>
2023-11-11 15:55:39 +0900

    feat : add dummy-data (#28)

---
+++ src/dummy/mySubjectData.ts
@@ -0,0 +1,73 @@
+export const mySubjectData = {
+  category: ["SPC", "쿠팡 노동자 사망", "후쿠시마 오염수", "이태원 참사"],
+  data: [
+    {
+      category: "SPC",
+      semidata: [
+        {
+          title: "SPC1",
+          type: "기사",
+          image: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+          date: "2023.10.24",
+        },
+        {
+          title: "SPC2",
+          type: "기사",
+          image: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+          date: "2023.10.24",
+        },
+      ],
+    },
+    {
+      category: "쿠팡 노동자 사망",
+      semidata: [
+        {
+          title: "쿠팡 노동자 사망1",
+          type: "기사",
+          image: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+          date: "2023.10.24",
+        },
+        {
+          title: "쿠팡 노동자 사망2",
+          type: "기사",
+          image: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+          date: "2023.10.24",
+        },
+      ],
+    },
+    {
+      category: "이태원 참사",
+      semidata: [
+        {
+          title: "이태원 참사1",
+          type: "기사",
+          image: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+          date: "2023.10.24",
+        },
+        {
+          title: "이태원 참사2",
+          type: "기사",
+          image: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+          date: "2023.10.24",
+        },
+      ],
+    },
+    {
+      category: "후쿠시마 오염수",
+      semidata: [
+        {
+          title: "후쿠시마 오염수1",
+          type: "기사",
+          image: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+          date: "2023.10.24",
+        },
+        {
+          title: "후쿠시마 오염수2",
+          type: "기사",
+          image: "https://gwanghwamun.seoul.go.kr/resources/client2022/images/bg_info_mo.jpg",
+          date: "2023.10.24",
+        },
+      ],
+    },
+  ],
+};
