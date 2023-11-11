78e4b80af1d89f1f4acf98b5bbb0f6af2f2c6828
chamny20 <chamny20@naver.com>
2023-11-12 05:15:59 +0900

    chore : add id in dummy-data (#16)

--- src/dummy/mainCommunityData.ts
+++ src/dummy/mainCommunityData.ts
@@ -1,5 +1,7 @@
 export const mainCommunityData = [
   {
+    id: 1,
+    profileImg: "",
     nickname: "111광장찌개",
     category: "교육",
     date: "2023.10.24",
@@ -7,26 +9,38 @@ export const mainCommunityData = [
     commentCount: 36,
     communityText:
       "일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는 비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며, 강제성이 있는 ",
-
     quotText: "와배고픈데또졸리고 자고싶지만 잘 수 없네 광화문 메운 검은 물결 하하하",
   },
   {
+    id: 2,
+    profileImg: "",
     nickname: "광장찌개",
     category: "교육",
-
     date: "2023.10.24",
     likeCount: 24,
     commentCount: 36,
     communityText:
       "일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는 비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며, 강제성이 있는 ",
-
     quotText:
       "와배고픈데sdfsdfffffffffffffffffefffffffffffsefs미ㅑㅓㄷ먀ㅑㅑㅑㅑㅑㅑㅑㅑㅑㅑㅑ또졸리고 자고싶지만 잘 수 없네 광화문 메운 검은 물결 하하하",
   },
   {
+    id: 3,
+    profileImg: "",
     nickname: "광장찌개",
     category: "교육",
-
+    date: "2023.10.24",
+    likeCount: 24,
+    commentCount: 36,
+    communityText:
+      "일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는 비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며, 강제성이 있는 ",
+    quotText: "와배고픈데또졸리고 자고싶지만 잘 수 없네 광화문 메운 검은 물결 하하하",
+  },
+  {
+    id: 4,
+    profileImg: "",
+    nickname: "광장찌개",
+    category: "교육",
     date: "2023.10.24",
     likeCount: 24,
     commentCount: 36,
@@ -36,51 +50,39 @@ export const mainCommunityData = [
     quotText: "와배고픈데또졸리고 자고싶지만 잘 수 없네 광화문 메운 검은 물결 하하하",
   },
   {
+    id: 5,
+    profileImg: "",
     nickname: "광장찌개",
     category: "교육",
-
     date: "2023.10.24",
     likeCount: 24,
     commentCount: 36,
     communityText:
       "일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는 비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며, 강제성이 있는 ",
-
     quotText: "와배고픈데또졸리고 자고싶지만 잘 수 없네 광화문 메운 검은 물결 하하하",
   },
   {
+    id: 7,
+    profileImg: "",
     nickname: "광장찌개",
     category: "교육",
-
     date: "2023.10.24",
     likeCount: 24,
     commentCount: 36,
     communityText:
       "일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는 비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며, 강제성이 있는 ",
-
     quotText: "와배고픈데또졸리고 자고싶지만 잘 수 없네 광화문 메운 검은 물결 하하하",
   },
   {
-    nickname: "광장찌개",
-    category: "교육",
-
-    date: "2023.10.24",
-    likeCount: 24,
-    commentCount: 36,
-    communityText:
-      "일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는 비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며, 강제성이 있는 ",
-
-    quotText: "와배고픈데또졸리고 자고싶지만 잘 수 없네 광화문 메운 검은 물결 하하하",
-  },
-  {
+    id: 8,
+    profileImg: "",
     nickname: "last",
     category: "교육",
-
     date: "2023.10.24",
     likeCount: 24,
     commentCount: 36,
     communityText:
       "일기란 개인이 일상에서 체험하는 경험, 생각, 감상 등의 제반사항을 하루 단위로 기록하는 비공식적, 사적 기록이다. 보통 일일 단위로 기록하지만, 작성하는 개인에 따라 천차만별이며, 강제성이 있는 ",
-
     quotText: "와배고픈데또졸리고 자고싶지만 잘 수 없네 광화문 메운 검은 물결 하하하",
   },
 ];
