daeaeaa9a1fe26bd3f5697d622a85d00aab3e1db
chamny20 <chamny20@naver.com>
2023-11-11 15:56:46 +0900

    feat : mapping dummy-data (#28)

---
+++ src/components/organisms/Home/LoginTopic.tsx
@@ -0,0 +1,14 @@
+// import { mySubjectData } from "@/dummy/mySubjectData";
+
+export const LoginTopic = ({ selectedKeyword }: { selectedKeyword: string }) => {
+  console.log(selectedKeyword);
+
+  //선택된 카테고리(키워드)에 따라 데이터 뽑기
+  //   const filteredData = mySubjectData.find((item) => item.keyword === selectedKeyword);
+
+  //   if (!filteredData) {
+  //     console.error(`No data found for the keyword: ${selectedKeyword}`);
+  //     return null;
+  //   }
+  return <></>;
+};
