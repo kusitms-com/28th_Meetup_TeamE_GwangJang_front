bb4e143b31fda456ee068d8541a04ec2784748c0
hoyyChoi <hoyeon8165@gmail.com>
2023-11-13 05:09:37 +0900

    feat : kakaoLogin api

--- src/components/molecules/login/KakaoCallBack.tsx
+++ src/components/molecules/login/KakaoCallBack.tsx
@@ -1,48 +1,32 @@
 import { useEffect } from "react";
 
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { SyncLoader } from "react-spinners";
-import styled from "styled-components";
+
+import { postKakaoLogin } from "@/apis";
+import Loading from "@/components/atoms/Loading";
 
 export const KakaoCallBack = () => {
   const navigate = useNavigate();
+
   useEffect(() => {
     const Code = new URL(window.location.href).searchParams.get("code");
-    console.log("인가코드:", Code);
-    axios
-      .post(
-        "https://api.gwang-jang.co.kr/member/auth/signIn/kakao",
-        { token: Code },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
+    postKakaoLogin(Code)
       .then((res) => {
-        console.log(res); // res -> "registrationStatus": true navigate / 라우팅 false 이면  navigate /addInfomation
-        navigate("/");
+        localStorage.setItem("accessToken", res.data.data.accessToken);
+        localStorage.setItem("refreshToken", res.data.data.refreshToken);
+        if (res.data.data.registrationStatus) {
+          navigate("/");
+        } else {
+          navigate("/addInfomation");
+        }
+        // res -> "registrationStatus": true navigate / 라우팅 false 이면  navigate /addInfomation
+        //navigate("/");
       })
       .catch((err) => {
-        console.error("error:", err);
+        alert(err.response.data.message);
+        navigate("/login");
       });
   }, [navigate]);
 
-  return (
-    <LoadingBox>
-      <h3>잠시만 기다려주세요.</h3>
-      <SyncLoader />
-    </LoadingBox>
-  );
+  return <Loading />;
 };
-
-const LoadingBox = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  flex-direction: column;
-  gap: 2rem;
-  font-size: 1.3rem;
-`;
