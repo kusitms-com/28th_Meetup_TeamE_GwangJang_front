9041b0dcd8950fa6e08339ad97cd522a3a2ff210
chamny20 <chamny20@naver.com>
2023-11-13 01:07:01 +0900

    feat : apply props (#28)

--- src/components/molecules/slideItem/index.tsx
+++ src/components/molecules/slideItem/index.tsx
@@ -1,19 +1,22 @@
+import temp from "@/assets/main_logo.svg";
+import { SlideItemProps } from "@/types";
+
 import { SlideWrapper } from "./style";
 
-export const SlideItem = () => {
+export const SlideItem = ({ title, type, date }: SlideItemProps) => {
   return (
     <SlideWrapper>
       <div className="slide-container">
         <div className="slide-image">
           <img
-            // src={}
+            src={temp}
             alt="썸네일"
           />
         </div>
         <div className="slide-text">
-          <div className="text-type">type</div>
-          <div className="text-title">title</div>
-          <div className="text-date">date</div>
+          <div className="text-type">{type}</div>
+          <div className="text-title">{title}</div>
+          <div className="text-date">{date}</div>
         </div>
       </div>
     </SlideWrapper>
