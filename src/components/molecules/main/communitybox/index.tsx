cefb4fcd5c15769fd31444d846627412a0f2d234
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-12 20:26:27 +0900

    fix : swiper breakpoint

--- src/components/molecules/main/communitybox/index.tsx
+++ src/components/molecules/main/communitybox/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -13,21 +13,21 @@ import { CommunityContainer } from "./style";
 import "swiper/css";
 
 export const CommunityBox = () => {
-  const [margin, setMargin] = useState((window.innerWidth - 1080) * 0.49);
+  // const [margin, setMargin] = useState((window.innerWidth - 1080) * 0.49);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setMargin(window.innerWidth * 0.25 * 0.5);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setMargin(window.innerWidth * 0.25 * 0.5);
+  //   };
 
-    // 이벤트 리스너 등록
-    window.addEventListener("resize", handleResize);
+  //   // 이벤트 리스너 등록
+  //   window.addEventListener("resize", handleResize);
 
-    // 컴포넌트 언마운트 시 이벤트 리스너 제거
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  //   // 컴포넌트 언마운트 시 이벤트 리스너 제거
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
 
   return (
     <TempContainer>
@@ -35,32 +35,101 @@ export const CommunityBox = () => {
         loop={false}
         spaceBetween={24}
         breakpoints={{
-          344: {
+          300: {
             slidesPerView: 1,
-            // spaceBetween: 24,
+            slidesOffsetBefore: 20,
+            spaceBetween: 44,
           },
-          688: {
+          450: {
+            slidesPerView: 1.5,
+            slidesOffsetBefore: 20,
+            spaceBetween: 44,
+          },
+          540: {
+            slidesPerView: 1.7,
+            slidesOffsetBefore: 20,
+            spaceBetween: 44,
+          },
+          638: {
+            slidesPerView: 2,
+            slidesOffsetBefore: 20,
+            spaceBetween: 44,
+          },
+          820: {
             slidesPerView: 2.5,
-            // spaceBetween: 24,
+            slidesOffsetBefore: 10,
+            spaceBetween: 44,
+          },
+          1000: {
+            slidesPerView: 3,
+            slidesOffsetBefore: 10,
+            spaceBetween: 44,
           },
           1080: {
-            slidesPerView: 3.5,
-            // spaceBetween: 24,
+            slidesPerView: 3,
+            slidesOffsetBefore: 10,
+            spaceBetween: 44,
           },
-          1376: {
+          1150: {
+            slidesPerView: 3.3,
+            slidesOffsetBefore: 20,
+            spaceBetween: 44,
+          },
+          1220: {
+            slidesPerView: 3.5,
+            slidesOffsetBefore: 80,
+            spaceBetween: 44,
+          },
+          1280: {
+            slidesPerView: 3.5,
+            slidesOffsetBefore: 110,
+            spaceBetween: 44,
+          },
+          1340: {
+            slidesPerView: 3.5,
+            slidesOffsetBefore: 130,
+            spaceBetween: 44,
+          },
+          1380: {
+            slidesPerView: 4,
+            slidesOffsetBefore: 150,
+            spaceBetween: 44,
+          },
+          1420: {
+            slidesPerView: 4,
+            slidesOffsetBefore: 170,
+            spaceBetween: 44,
+          },
+          1490: {
+            slidesPerView: 4,
+            slidesOffsetBefore: 200,
+            spaceBetween: 44,
+          },
+          1550: {
+            slidesPerView: 4,
+            slidesOffsetBefore: 240,
+            spaceBetween: 44,
+          },
+          1610: {
+            slidesPerView: 4,
+            slidesOffsetBefore: 260,
+            spaceBetween: 44,
+          },
+          1650: {
             slidesPerView: 4.5,
-            // spaceBetween: 24,
+            slidesOffsetBefore: 290,
+            spaceBetween: 44,
           },
           1720: {
             slidesPerView: 5,
-            // spaceBetween: 24,
+            spaceBetween: 44,
+            slidesOffsetBefore: 310,
           },
         }}
       >
         {mainCommunityData.map((item, idx) => (
           <SwiperSlide
             key={idx}
-            style={idx == 0 ? { marginLeft: `${margin}px` } : {}}
           >
             <CommunityContainer>
               <div className="community-content">
