0ae570cdf04d7f977897735090f972d72b01c542
chamny20 <chamny20@naver.com>
2023-11-13 22:11:35 +0900

    feat : apply recoilvalue (#28)

--- src/components/organisms/Home/LoginTopic.tsx
+++ src/components/organisms/Home/LoginTopic.tsx
@@ -1,41 +1,29 @@
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 
 import { CategoryFilter } from "@/components/molecules/categoryFilter";
 import { SlideItem } from "@/components/molecules/slideItem";
-import { mySubjectData } from "@/dummy/mySubjectData";
+import { filteredDataSelector } from "@/recoil/atoms";
 
-export const LoginTopic = ({ selectedKeyword }: { selectedKeyword: string }) => {
-  console.log(selectedKeyword);
-
-  //선택된 카테고리(키워드)에 따라 데이터 뽑기
-  // const filteredData = mySubjectData.find((item) => item.keyword === selectedKeyword);
-  console.log(mySubjectData);
-
-  //   if (!filteredData) {
-  //     console.error(`No data found for the keyword: ${selectedKeyword}`);
-  //     return null;
-  //   }
+export const LoginTopic = () => {
+  const filteredData = useRecoilValue(filteredDataSelector);
 
   return (
     <>
       <div>
         <CategoryFilter />
       </div>
-      {mySubjectData.data.map((item, idx) => (
-        <SlideContainer key={idx}>
-          {/* {item.category == selectedKeyword && {
-            }} */}
-          {item.semiData.map((data, idx) => (
-            <SlideItemWrapper key={idx}>
-              <SlideItem
-                title={data.title}
-                type={data.type}
-                date={data.date}
-              />
-            </SlideItemWrapper>
-          ))}
-        </SlideContainer>
-      ))}
+      <SlideContainer>
+        {filteredData.map((data, idx) => (
+          <SlideItemWrapper key={idx}>
+            <SlideItem
+              title={data.title}
+              type={data.type}
+              date={data.date}
+            />
+          </SlideItemWrapper>
+        ))}
+      </SlideContainer>
     </>
   );
 };
