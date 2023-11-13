6de2d03bed90e3bfd05273b9279f263eca372557
chamny20 <chamny20@naver.com>
2023-11-13 23:32:14 +0900

    feat : add slider-setting and apply (#28)

--- src/components/organisms/Home/LoginTopic.tsx
+++ src/components/organisms/Home/LoginTopic.tsx
@@ -1,5 +1,9 @@
+import Slider from "react-slick";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { CategoryFilter } from "@/components/molecules/categoryFilter";
 import { SlideItem } from "@/components/molecules/slideItem";
@@ -8,32 +12,49 @@ import { filteredDataSelector } from "@/recoil/atoms";
 export const LoginTopic = () => {
   const filteredData = useRecoilValue(filteredDataSelector);
 
+  const maxSlidesToShow = Math.min(filteredData.length, 4);
+
+  const SliderSetting = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: maxSlidesToShow,
+  };
+
   return (
     <>
-      <div>
+      <CategorySlideWrapper>
         <CategoryFilter />
-      </div>
-      <SlideContainer>
-        {filteredData.map((data, idx) => (
-          <SlideItemWrapper key={idx}>
-            <SlideItem
-              title={data.title}
-              type={data.type}
-              date={data.date}
-            />
-          </SlideItemWrapper>
-        ))}
-      </SlideContainer>
+        <StyledSlider {...SliderSetting}>
+          {filteredData.map((data, idx) => (
+            <SlideItemWrapper key={idx}>
+              <SlideItem
+                title={data.title}
+                type={data.type}
+                date={data.date}
+              />
+            </SlideItemWrapper>
+          ))}
+        </StyledSlider>
+      </CategorySlideWrapper>
     </>
   );
 };
 
-const SlideContainer = styled.div`
+const StyledSlider = styled(Slider)`
+  width: 1080px;
   display: flex;
+  margin: 0 auto;
   gap: 24px;
   background-color: pink;
 `;
 
 const SlideItemWrapper = styled.div`
   display: flex;
+`;
+
+const CategorySlideWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
 `;
