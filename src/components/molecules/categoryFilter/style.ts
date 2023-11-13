ef8977c82c05d7278a4a20cf60705a6047b5e46c
chamny20 <chamny20@naver.com>
2023-11-13 23:31:46 +0900

    design : modified css about middle sorting (#28)

--- src/components/molecules/categoryFilter/style.ts
+++ src/components/molecules/categoryFilter/style.ts
@@ -2,5 +2,7 @@ import styled from "styled-components";
 
 export const CategoryFilterContainer = styled.div`
   display: flex;
+  width: 1080px;
+  margin: 0 auto;
   gap: 8px;
 `;
--- src/components/molecules/slideItem/style.ts
+++ src/components/molecules/slideItem/style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const SlideWrapper = styled.div`
   display: flex;
+  justify-content: center;
   gap: 24px;
 
   .slide-container {
