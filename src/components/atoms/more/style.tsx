e0d9ec98f7362ce2b941d55c995c3aca5a784dd5
chamny20 <chamny20@naver.com>
2023-11-14 04:26:57 +0900

    feat : modified see-more atom css (#28)

--- src/components/atoms/more/style.tsx
+++ src/components/atoms/more/style.tsx
@@ -2,15 +2,20 @@ import styled from "styled-components";
 
 export const MoreBox = styled.div`
   display: flex;
+  justify-content: flex-end;
   flex-direction: row;
   color: var(--Gray7_600, #757575);
-  align-items: center;
   font-size: 16px;
-  justify-content: right;
   cursor: pointer;
 
-  .next-icon {
-    color: inherit;
-    font-size: 20px;
+  .onClick-item {
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    .next-icon {
+      color: inherit;
+      font-size: 20px;
+    }
   }
 `;
