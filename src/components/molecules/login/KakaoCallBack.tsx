0664573fc7329b7d51f4fd8cc1421ca36035df09
hoyyChoi <hoyeon8165@gmail.com>
2023-11-13 14:00:46 +0900

    feat : kakao addinfo Api

--- src/components/molecules/login/KakaoCallBack.tsx
+++ src/components/molecules/login/KakaoCallBack.tsx
@@ -1,22 +1,27 @@
 import { useEffect } from "react";
 
 import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
 
 import { postKakaoLogin } from "@/apis";
 import Loading from "@/components/atoms/Loading";
+import { accessTokenState } from "@/recoil/atoms";
 
 export const KakaoCallBack = () => {
   const navigate = useNavigate();
+  const setAccessToken = useSetRecoilState(accessTokenState);
 
   useEffect(() => {
     const Code = new URL(window.location.href).searchParams.get("code");
     postKakaoLogin(Code)
       .then((res) => {
-        localStorage.setItem("accessToken", res.data.data.accessToken);
-        localStorage.setItem("refreshToken", res.data.data.refreshToken);
+        console.log(res);
         if (res.data.data.registrationStatus) {
+          localStorage.setItem("accessToken", res.data.data.accessToken);
+          localStorage.setItem("refreshToken", res.data.data.refreshToken);
           navigate("/");
         } else {
+          setAccessToken(res.data.data.accessToken);
           navigate("/addInfomation");
         }
         // res -> "registrationStatus": true navigate / 라우팅 false 이면  navigate /addInfomation
@@ -26,7 +31,7 @@ export const KakaoCallBack = () => {
         alert(err.response.data.message);
         navigate("/login");
       });
-  }, [navigate]);
+  }, [navigate, setAccessToken]);
 
   return <Loading />;
 };
