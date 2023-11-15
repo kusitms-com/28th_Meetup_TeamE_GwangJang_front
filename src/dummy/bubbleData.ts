30c6a7d3b807a2bd8797fb2c4c46a41145b30496
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-16 04:26:15 +0900

    feat : bubbleGraph dummy data

---
+++ src/dummy/bubbleData.ts
@@ -0,0 +1,9 @@
+export const bubbleDummydata = [
+  { x: 0, y: null, z: 0, name: "" },
+  { x: 6, y: 1, z: 93, name: "피폭" },
+  { x: 2, y: 3, z: 143, name: "해양투기" },
+  { x: 5, y: 4, z: 113, name: "방사능" },
+  { x: 9, y: 5, z: 53, name: "수산물" },
+  { x: 11, y: 8, z: 203, name: "3차 방류" },
+  { x: 13, y: null, z: 0, name: "" },
+];
