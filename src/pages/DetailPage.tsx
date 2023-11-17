00078bf3119feb460e7f776c5406075773c8ab90
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-18 02:44:55 +0900

    feat : detailPage component sort

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,13 +1,18 @@
 import BubbleGraph from "@/components/organisms/Detail/BubbleGraph";
 import DetailArticleTitle from "@/components/organisms/Detail/DetailArticleTitle";
+import { KeywordArticle } from "@/components/organisms/Detail/KeywordArticle";
 import LineGraph from "@/components/organisms/Detail/LineGraph";
-
+import { KeywordVideo } from "@/components/organisms/Detail/keywordVideo";
+import { CommunityPreview } from "@/components/organisms/SubjectDetail/CommunityPreview";
 const DetailPage = () => {
   return (
     <>
       <DetailArticleTitle />
       <BubbleGraph />
       <LineGraph />
+      <KeywordArticle />
+      <KeywordVideo />
+      <CommunityPreview />
     </>
   );
 };
