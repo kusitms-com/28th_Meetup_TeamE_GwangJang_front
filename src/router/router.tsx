6969e08ef86ec60e88ad57936c1aecb35363b8bc
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-17 23:02:49 +0900

    feat : routing id

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -39,7 +39,7 @@ const router = () => {
             element={<Home />}
           />
           <Route
-            path="/detail"
+            path="/detail/:id"
             element={<DetailPage />}
           />
         </Route>
