e845f7df43547192a774eb966530ee018f1c6523
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-17 17:27:39 +0900

    chore : test Routing

--- src/components/organisms/Common/Header.tsx
+++ src/components/organisms/Common/Header.tsx
@@ -75,7 +75,7 @@ const Header = () => {
               </div>
               <div
                 className="register"
-                onClick={() => SpaceTo("/detail")}
+                onClick={() => SpaceTo("/register")}
               >
                 회원가입
               </div>
