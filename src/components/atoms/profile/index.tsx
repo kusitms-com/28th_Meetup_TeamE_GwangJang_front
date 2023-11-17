5279598aabda533eac6395490bc9666c664bbe33
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-17 19:06:34 +0900

    feat : longProfile component

--- src/components/atoms/profile/index.tsx
+++ src/components/atoms/profile/index.tsx
@@ -1,7 +1,7 @@
 import temp from "@/assets/react.svg";
 import { ProfileProps } from "@/types";
 
-import { UserInfo } from "./style";
+import { LongUserInfo, UserInfo } from "./style";
 
 export const Profile = ({ nickname, profileImg, date }: ProfileProps) => {
   profileImg = temp;
@@ -10,12 +10,27 @@ export const Profile = ({ nickname, profileImg, date }: ProfileProps) => {
     <UserInfo>
       <img
         src={profileImg}
-        alt=""
+        alt="O"
       />
       <div>
         <p className="user-nickname">{nickname}</p>
         <p className="content-date">{date}</p>
       </div>{" "}
     </UserInfo>
+  );
+};
+
+export const LongProfile = ({ nickname, profileImg, date }: ProfileProps) => {
+  profileImg = temp;
+
+  return (
+    <LongUserInfo>
+      <img
+        src={profileImg}
+        alt="O"
+      />
+      <p className="user-nickname">{nickname}</p>
+      <p className="content-date">{date}</p>
+    </LongUserInfo>
   );
 };
