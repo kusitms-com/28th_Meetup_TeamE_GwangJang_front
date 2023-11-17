b8a0776b9c326b3aceb84bf798b1e834d9717583
chamny20 <chamny20@naver.com>
2023-11-18 02:53:44 +0900

    chore : resolve vercel error

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,9 +1,10 @@
+import { CommunityPreview } from "@/components/organisms/SubjectDetail/CommunityPreview";
 import BubbleGraph from "@/components/organisms/detail/BubbleGraph";
 import DetailArticleTitle from "@/components/organisms/detail/DetailArticleTitle";
 import { KeywordArticle } from "@/components/organisms/detail/KeywordArticle";
-import { KeywordVideo } from "@/components/organisms/detail/keywordVideo";
 import LineGraph from "@/components/organisms/detail/LineGraph";
-import { CommunityPreview } from "@/components/organisms/SubjectDetail/CommunityPreview";
+import { KeywordVideo } from "@/components/organisms/detail/keywordVideo";
+
 const DetailPage = () => {
   return (
     <>
