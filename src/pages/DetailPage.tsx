6ace61798d7f46474b37ee8a022372c3da2f2ddf
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-18 03:37:05 +0900

    chore : conflict

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,8 +1,8 @@
-import { BubbleGraph } from "@/components/organisms/Detail/BubbleGraph";
-import { DetailArticleTitle } from "@/components/organisms/Detail/DetailArticleTitle";
-import { KeywordArticle } from "@/components/organisms/Detail/KeywordArticle";
-import { LineGraph } from "@/components/organisms/Detail/LineGraph";
-import { KeywordVideo } from "@/components/organisms/Detail/keywordVideo";
+import { BubbleGraph } from "@/components/organisms/Details/BubbleGraph";
+import { DetailArticleTitle } from "@/components/organisms/Details/DetailArticleTitle";
+import { KeywordArticle } from "@/components/organisms/Details/KeywordArticle";
+import { LineGraph } from "@/components/organisms/Details/LineGraph";
+import { KeywordVideo } from "@/components/organisms/Details/keywordVideo";
 import { CommunityPreview } from "@/components/organisms/SubjectDetail/CommunityPreview";
 
 const DetailPage = () => {
