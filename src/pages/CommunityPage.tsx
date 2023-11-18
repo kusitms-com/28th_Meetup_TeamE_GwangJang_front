8646cad3e0b14247192ec31e6cbe3fa38962cf90
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-18 20:07:34 +0900

    feat : CommunityPage arrangement

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
-import { CategoryBar } from "@/components/molecules/categoryBar";
-import { DetailCommunityBox } from "@/components/molecules/detailCommunityBox";
-import { CommunityMainList } from "@/components/organisms/SubjectDetail/CommunityMainList";
-import { envirData } from "@/dummy/AreaData";
+import styled from "styled-components";
 
+import { CategoryBar } from "@/components/molecules/categoryBar";
+import { CommunityMainList } from "@/components/organisms/Community/CommunityMainList";
+import CommunityPopular from "@/components/organisms/Community/CommunityPopular";
+import CommunityTitle from "@/components/organisms/Community/CommunityTitle";
+import CommunityTopTopic from "@/components/organisms/Community/CommunityTopTopic";
+import { PopularCommunityData } from "@/dummy/PopularCommunityData";
+import { ToptopicData } from "@/dummy/ToptopicData";
 export const CommunityPage = () => {
   const [selectedTab, setSelectedTab] = useState(0);
 
@@ -14,15 +18,28 @@ export const CommunityPage = () => {
 
   return (
     <>
+      <CommunityTitle />
       <CategoryBar onSelectTab={handleTabChange} />
-      <CommunityMainList selectedTab={selectedTab} />
-      <br />
-      {envirData.map((item, idx) => (
-        <DetailCommunityBox
-          key={idx}
-          data={item}
-        />
-      ))}
+      <Bottom>
+        <div className="inner">
+          <CommunityMainList selectedTab={selectedTab} />
+          <div className="sideCommunity">
+            <CommunityPopular data={PopularCommunityData} />
+            <CommunityTopTopic data={ToptopicData} />
+          </div>
+        </div>
+      </Bottom>
     </>
   );
 };
+
+export const Bottom = styled.div`
+  width: 100%;
+  background-color: var(--Gray3_200, #eee);
+  .inner {
+    width: 1080px;
+    margin: 0 auto;
+    display: flex;
+    padding-bottom: 152px;
+  }
+`;
