80694fad1959ff968214151194246e1d45e39807
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-18 20:08:15 +0900

    rename : communityPreview

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,9 +1,9 @@
 import { BubbleGraph } from "@/components/organisms/Details/BubbleGraph";
+import { CommunityPreview } from "@/components/organisms/Details/CommunityPreview";
 import { DetailArticleTitle } from "@/components/organisms/Details/DetailArticleTitle";
 import { KeywordArticle } from "@/components/organisms/Details/KeywordArticle";
 import { LineGraph } from "@/components/organisms/Details/LineGraph";
 import { KeywordVideo } from "@/components/organisms/Details/keywordVideo";
-import { CommunityPreview } from "@/components/organisms/SubjectDetail/CommunityPreview";
 
 const DetailPage = () => {
   return (
