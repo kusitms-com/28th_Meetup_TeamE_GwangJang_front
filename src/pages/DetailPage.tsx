d16fe2dffbf02e3dd04b3728e4cc719efd90d353
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2023-11-18 15:37:10 +0900

    feat : detailPage arrangement

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,9 +1,13 @@
+import styled from "styled-components";
+
 import { BubbleGraph } from "@/components/organisms/Details/BubbleGraph";
 import { DetailArticleTitle } from "@/components/organisms/Details/DetailArticleTitle";
 import { KeywordArticle } from "@/components/organisms/Details/KeywordArticle";
 import { LineGraph } from "@/components/organisms/Details/LineGraph";
+import SimilarTopic from "@/components/organisms/Details/SimilarTopic";
 import { KeywordVideo } from "@/components/organisms/Details/keywordVideo";
 import { CommunityPreview } from "@/components/organisms/SubjectDetail/CommunityPreview";
+import { similartopicData } from "@/dummy/similartopicData";
 
 const DetailPage = () => {
   return (
@@ -13,9 +17,22 @@ const DetailPage = () => {
       <LineGraph />
       <KeywordArticle />
       <KeywordVideo />
-      <CommunityPreview />
+      <Bottom>
+        <CommunityPreview />
+        <SimilarTopic data={similartopicData} />
+      </Bottom>
     </>
   );
 };
 
 export default DetailPage;
+
+const Bottom = styled.div`
+  background-color: var(--Gray2_100);
+  padding-top: 45px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 24px;
+  padding-bottom: 66px;
+`;
