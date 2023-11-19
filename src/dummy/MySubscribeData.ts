e85e266fb91ddd2c4fc5e84f6d286d8684501115
chamny20 <chamny20@naver.com>
2023-11-19 20:18:55 +0900

    chore : add dummy-my-subscribe-data (#60)

---
+++ src/dummy/MySubscribeData.ts
@@ -0,0 +1,17 @@
+export const MySubscribeData = [
+  {
+    id: 8,
+    area: "환경",
+    subject: "후쿠시마 오염수",
+  },
+  {
+    id: 2,
+    area: "환경",
+    subject: "후쿠시마 오염수",
+  },
+  {
+    id: 3,
+    area: "환경",
+    subject: "후쿠시마 오염수",
+  },
+];
