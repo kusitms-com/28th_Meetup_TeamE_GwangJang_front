4d2fb9a376d505a2126499e9a65a67e536ec7bc4
chamny20 <chamny20@naver.com>
2023-11-19 19:32:50 +0900

    feat : add my post (#60)

---
+++ src/components/organisms/MyPage/MyPost.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+import { BigProfile } from "@/components/atoms/profile";
+import { MainCommunityBox } from "@/components/molecules/mainCommunityBox";
+import { envirData } from "@/dummy/AreaData";
+
+export const MyPost = () => {
+  return (
+    <>
+      {envirData.map((item, idx) => (
+        <MyPostWrapper>
+          <BigProfile
+            nickname={item.nickname}
+            date={item.date}
+            profileImg={item.profileImg}
+          />
+          <MainCommunityBox
+            key={idx}
+            data={item}
+          />
+        </MyPostWrapper>
+      ))}
+    </>
+  );
+};
+
+export const MyPostWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
