d6e5706bfefec4e4450ba12131768d54b53780e7
chamny20 <chamny20@naver.com>
2023-11-23 18:44:58 +0900

    feat : filter tab

--- src/components/organisms/MyPage/MyPost.tsx
+++ src/components/organisms/MyPage/MyPost.tsx
@@ -1,14 +1,33 @@
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 
 import { BigProfile } from "@/components/atoms/profile";
 import { MainCommunityBox } from "@/components/molecules/mainCommunityBox";
-import { envirData } from "@/dummy/AreaData";
+// import { envirData } from "@/dummy/AreaData";
+import { myLikeData, myWriteData, selectedTabState } from "@/recoil/atoms";
 
 export const MyPost = () => {
+  const myLike = useRecoilValue(myLikeData);
+  // console.log("s", myLike);
+  const myWrite = useRecoilValue(myWriteData);
+
+  const selectedTab = useRecoilValue(selectedTabState);
+
+  let data: (typeof myWrite)[number][] | (typeof myLike)[number][] = [];
+
+  if (selectedTab === 0) {
+    data = myWrite;
+    console.log(data);
+  } else if (selectedTab === 2) {
+    data = myLike;
+  }
+
+  console.log("데이터:", data);
+
   return (
     <Container>
-      {envirData.map((item, idx) => (
-        <MyPostWrapper>
+      {data.map((item, idx) => (
+        <MyPostWrapper key={idx}>
           <BigProfile
             nickname={item.nickname}
             date={item.date}
